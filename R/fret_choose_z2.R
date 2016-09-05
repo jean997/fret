15616de6f083141482d8be1c1bbd03b0cc5ca484
Jean Morrison <jeanm5@uw.edu>
2016-09-05 13:52:29 -0700

    get thresholds for target fdr values

--- R/fret_choose_z2.R
+++ R/fret_choose_z2.R
@@ -1,15 +1,20 @@
+#'@import intervals
 
-#' Find thresholds for a range of lambda values. Calculate FDR.
+
+#'@title Find thresholds for a range of lambda values. Calculate FDR.
 #'@description Find thresholds for a range of lambda values. Calculate FDR.
 #'@param max1.list List of peak heights for each segment.
 #'@param perm.maxes.list a list of matrices giving peak heights for permutations. Each matrix has two columns. First column is the threshold level,
 #'second column is lambda per bp (num of peaks less than threshold divided by number of permutations and number of base-pairs).
 #'@param nbp Number of base-pairs per segment
-#'@param nlam Number of lambda values to consider
+#'@param zmin
+#'@param fdr.max
+#'@param seg.names
+#'@param target.fdr
 #' @return A list with items z, Robs, and fdr.
 #'@export
 fret_choose_z2 <- function(max1.list, perm.maxes.list, nbp, zmin, fdr.max=0.8,
-                            seg.names=NULL){
+                            seg.names=NULL, target.fdr=NULL){
 
   stopifnot(length(perm.maxes.list)==length(nbp))
   stopifnot(length(perm.maxes.list)==length(max1.list))
@@ -17,7 +22,7 @@ fret_choose_z2 <- function(max1.list, perm.maxes.list, nbp, zmin, fdr.max=0.8,
   s <- length(zmin)
 
   K <- length(max1.list)
-
+  if(is.null(seg.names)) seg.names <- paste0("segment", 1:K)
   m1tab <- matrix(nrow=0, ncol=3)
   for(i in 1:K){
     m1 <- sort(max1.list[[i]], decreasing=TRUE)
@@ -45,14 +50,13 @@ fret_choose_z2 <- function(max1.list, perm.maxes.list, nbp, zmin, fdr.max=0.8,
   m1tab$sgn[m1tab$m1 < 0] <- 2
   names(m1tab)[3] <- "seg"
   m1tab <- m1tab[order(m1tab$rts, decreasing=FALSE), ]
+  m1tab$lambda <- 10^(m1tab$rts)*sum(nbp)*s
+  m1tab$fdr <- m1tab$lambda/(1:nrow(m1tab))
+  ix <- max(which(m1tab$fdr <= fdr.max)) + 1
 
-
-  lambda <- 10^(m1tab$rts)*sum(nbp)*s
-  fdr <- lambda/(1:nrow(m1tab))
-  ix <- max(which(fdr <= fdr.max)) + 1
-  fdr <- fdr[1:ix]
-  z <- Robs <- matrix(nrow=ix, ncol=K+1)
-  z[,1] <- Robs[,1] <- lambda[1:ix]
+  z <- Robs <- matrix(nrow=ix, ncol=K+2)
+  z[,1] <- Robs[,1] <- m1tab$lambda[1:ix]
+  z[,2] <- Robs[,2] <- m1tab$fdr[1:ix]
   if(s==1){
     zneg <- NULL
   }else{
@@ -62,36 +66,62 @@ fret_choose_z2 <- function(max1.list, perm.maxes.list, nbp, zmin, fdr.max=0.8,
   for(i in 1:ix){
     seg <- m1tab$seg[i]
     robs[seg] <- robs[seg] + 1
-    Robs[i,-1] <-robs
+    Robs[i,-c(1, 2)] <-robs
     #Thresholds
     if(s==2){
       zz <- get_thresh_sgn(perm.maxes.list, m1tab$rts[i], zmin)
-      z[i,-1] <- zz$zpos
-      zneg[i, -1] <- zz$zneg
+      z[i,-c(1, 2)] <- zz$zpos
+      zneg[i, -c(1, 2)] <- zz$zneg
     }else{
       zz <- get_thresh_usgn(perm.maxes.list, m1tab$rts[i], zmin)
-      z[i, -1] <- zz
+      z[i, -c(1, 2)] <- zz
     }
   }
-  ret <- list("Robs"=Robs, "z"=z, "zneg" = zneg, "fdr"=fdr, "nbp"=nbp, "m1tab"=m1tab)
+  if(!is.null(target.fdr)){
+    target.fdr <- target.fdr[target.fdr >= min(m1tab$fdr)]
+    for(ff in target.fdr){
+      ix.lower <- max(which(m1tab$fdr <= ff))
+      ix.upper <- ix.lower + 1
+      lam.target <- ff*ix.lower
+      log.lambda.pb <- log10(lam.target/(sum(nbp)*s))
+      if(log.lambda.pb > m1tab$rts[ix.upper]) stop("Something is wrong!\n")
+      Robs <- rbind(Robs, Robs[ix.lower,])
+      if(s==2){
+        zz <- get_thresh_sgn(perm.maxes.list, log.lambda.pb, zmin)
+        z <- rbind(z, c(lam.target, ff, zz$zpos))
+        zneg <- rbind(zneg, c(lam.target, ff, zz$zneg))
+      }else{
+        zz <- get_thresh_usgn(perm.maxes.list, log.lambda.pb, zmin)
+        z <- rbind(z, c(lam.target, ff, zz))
+      }
+    }
+  }
+  Robs <- data.frame(Robs)
+  z <- data.frame(z)
+  names(Robs) <- names(z) <- c("lambda", "fdr", seg.names)
+  if(s==2){
+    zneg <- data.frame(zneg)
+    names(zneg) <- names(z)
+  }
+  ret <- list("Robs"=Robs, "z"=z, "zneg" = zneg, "nbp"=nbp, "m1tab"=m1tab)
   return(ret)
 }
 
-get_thresh_usgn <- function(perm.maxes.list, log_lambda_bp, zmin, eps=1e-6){
+get_thresh_usgn <- function(perm.maxes.list, log.lambda.pb, zmin, eps=1e-6){
   z <- sapply(perm.maxes.list, FUN=function(m){
-    approx(x=log10(m[,2]), y=m[,1], xout=log_lambda_bp, yleft=Inf, yright=zmin)$y
+    approx(x=log10(m[,2]), y=m[,1], xout=log.lambda.pb, yleft=Inf, yright=zmin)$y
   })
   return(z-eps)
 }
 
-get_thresh_sgn <- function(perm.maxes.list, log_lambda_bp, zmin, eps=1e-6){
+get_thresh_sgn <- function(perm.maxes.list, log.lambda.pb, zmin, eps=1e-6){
   zpos <- sapply(perm.maxes.list, FUN=function(m){
     ix <- which(m[,1] > 0)
-    approx(x=log10(m[ix,2]), y=m[ix,1], xout=log_lambda_bp, yleft=Inf, yright=zmin[1])$y
+    approx(x=log10(m[ix,2]), y=m[ix,1], xout=log.lambda.pb, yleft=Inf, yright=zmin[1])$y
   })
   zneg <- sapply(perm.maxes.list, FUN=function(m){
     ix <- which(m[,1] < 0)
-    approx(x=log10(m[ix,2]), y=m[ix,1], xout=log_lambda_bp, yleft=-Inf, yright=zmin[2])$y
+    approx(x=log10(m[ix,2]), y=m[ix,1], xout=log.lambda.pb, yleft=-Inf, yright=zmin[2])$y
   })
   return(list("zpos"=zpos-eps, "zneg"=zneg-eps))
 }
