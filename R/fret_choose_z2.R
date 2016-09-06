9aa10b616104e63d989391292095cd01f411bf80
Jean Morrison <jeanm5@uw.edu>
2016-09-06 15:46:29 -0700

    what to do if no excursions at all

--- R/fret_choose_z2.R
+++ R/fret_choose_z2.R
@@ -52,12 +52,12 @@ fret_choose_z2 <- function(max1.list, perm.maxes.list, nbp, zmin, fdr.max=0.8,
   m1tab <- m1tab[order(m1tab$rts, decreasing=FALSE), ]
   m1tab$lambda <- m1tab$rts*sum(nbp)*s
   m1tab$fdr <- m1tab$lambda/(1:nrow(m1tab))
-  ix <- max(which(m1tab$fdr <= fdr.max)) + 1
-  if(length(ix)==0){
+  if(all(m1tab$fdr > fdr.max)){
     cat("No excursions with fdr value less than ", max.fdr, "\n")
     ret <- list("Robs"=NULL, "z"=NULL, "zneg" = NULL, "nbp"=nbp, "m1tab"=m1tab)
     return(ret)
   }
+  ix <- max(which(m1tab$fdr <= fdr.max)) + 1
   z <- Robs <- matrix(nrow=ix, ncol=K+2)
   z[,1] <- Robs[,1] <- m1tab$lambda[1:ix]
   z[,2] <- Robs[,2] <- m1tab$fdr[1:ix]
