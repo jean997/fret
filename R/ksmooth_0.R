a54bf4e214a30b3681faebaa8be72a12759e3acf
Jean Morrison <jeanm5@uw.edu>
2016-09-07 12:58:11 -0700

    Error messages

--- R/ksmooth_0.R
+++ R/ksmooth_0.R
@@ -8,6 +8,9 @@
 #'@return 2 by p matrix. Top row is coefficient estimate. Bottom row is sd estimates.
 #'@export
 ksmooth_0 <- function(x, y, bandwidth){
+  if(!all(floor(x)==x)) stop("ksmooth_0 should only be used with integer positions\n")
+  if(!floor(bandwidth)==bandwidth) stop("Please use an integer bandwidth with ksmooth_0\n")
+  if(any(is.na(y))) stop("No missing values please.\n")
   n <- length(y)
   y.out <- sapply(x, FUN=function(xx){
     sum(y[ x <= (xx+bandwidth/2) & x >= (xx - bandwidth/2)])/(bandwidth+1)
