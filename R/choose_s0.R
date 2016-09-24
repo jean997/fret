ca428f2c83a94366be063394b5358505c9d09f61
Jean Morrison <jeanm5@uw.edu>
2016-09-24 11:56:45 -0700

    change choose_s0

--- R/choose_s0.R
+++ R/choose_s0.R
@@ -1,16 +1,17 @@
 
 #'@export
 choose_s0 <- function(beta, sds){
-  salpha <- c(0, as.numeric(quantile(sds, probs=seq(0, 1, by=0.05))))
+  salpha <- c(0, as.numeric(quantile(sds, probs=seq(0, 1, by=0.01))))
   nn <- length(salpha)
   ix <- sapply(sds, FUN=function(w){ sum(w >= salpha[-nn])})
-  cvs <- c()
-  v <- as.numeric(by(data=beta/sds, INDICES = ix, FUN = mad, na.rm=TRUE))
-  cvs <- c(sd(v)/mean(v))
-  tol <- max(min(sds[sds > 0]), 1e-3)
 
-  fct <- function(s0, beta, sds, ix){
-    cat(s0, " ")
+  #v <- as.numeric(by(data=beta/sds, INDICES = ix, FUN = mad, na.rm=TRUE))
+  #cvs <- sd(v)/mean(v)
+  #tol <- max(min(sds[sds > 0]), 1e-3)
+
+  fct <- function(logs0, beta, sds, ix){
+    cat(logs0, " ")
+    s0 <- 10^(logs0)
     xx <- beta/(sds + s0)
     v <- as.numeric(by(data=xx, INDICES = ix, FUN = mad, constant = 1/0.64))
     cv <- sd(v)/mean(v)
@@ -18,8 +19,10 @@ choose_s0 <- function(beta, sds){
     return(cv)
   }
 
-  W <- optimize(fct, interval=c(0, max(sds)), beta=beta,
-                sds=sds, ix=ix, tol=tol)
+  W <- optimize(fct, interval=c(log10(min(sds))-1, max(log10(sds))), beta=beta,
+                sds=sds, ix=ix, tol=0.1)
+
+  #zz <- sapply(log10(salpha), FUN=fct, beta=beta, sds=sds, ix=ix)
   return(W$minimum)
 }
 
