c75859a1cdbcde6b881f39a53c3e4be5a93728a7
Jean Morrison <jeanm5@uw.edu>
2016-09-25 21:25:55 -0700

    No smoothing

--- R/find_segments.R
+++ R/find_segments.R
@@ -15,7 +15,7 @@
 #'@export
 find_segments <- function(vv, pos, min.length, z0=NULL, z=NULL, q=0.05){
   stopifnot(length(vv)==length(pos))
-  if(is.null(bandwidth)) bandwidth <- min.length/2
+  #if(is.null(bandwidth)) bandwidth <- min.length/2
   stopifnot(bandwidth > 0)
 
   #vvs <- ksmooth_0(x=pos, y=vv, xout=pos, bandwidth=bandwidth)
