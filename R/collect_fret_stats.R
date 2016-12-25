917f4e54d552ce2d5da593dd1d65e525801a99fe
Jean Morrison <jeanm@uchicago.edu>
2016-12-25 13:22:45 -0600

    Add documentation and min.interval.width option to collect_fret_stats function.

--- R/collect_fret_stats.R
+++ R/collect_fret_stats.R
@@ -1,6 +1,16 @@
+#' @title Collect fret_stats temporary files
+#' @description Combine temporary files from fret_stats into one file.
+#' Also determine interval boundaries.
+#' @param temp.dir Directory containing temporary files
+#' @param temp.prefix Prefix of temporary files (e.g. If your files are named
+#' test_chr1.N.RData where N is the chunk number then temp.prefix is test_chr1)
+#' @param which.chunk Vector of integers. Which chunks to collect
+#' @param out.file Name an output file
+#' @param del.temp Delete the temporary files when done?
+#' @param min.interval.width Minimum interval length when automatically determining interval endpoints
 #'@export
 collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
-                               out.file=NULL, del.temp=FALSE){
+                               out.file=NULL, del.temp=FALSE, min.interval.width=NULL){
   fl_exist <- list.files(temp.dir, pattern = temp.prefix, full.names = TRUE)
   fl <- paste0(temp.dir,"/", temp.prefix, ".", which.chunk, ".RData")
   stopifnot(all(fl %in% fl_exist))
@@ -44,6 +54,10 @@ collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
     R$mperm <- mperm
     R$perm.var <- perm.var
   }
+  if(is.null(min.interval.width)) min.interval.width=50*R$bandwidth
+  sb <- find_segments(vv=R$perm.var$var, pos=R$perm.var$pos, min.length=min.interval.width)
+  R$seg.bounds <- data.frame("chr"=rep(R$chrom, nrow(sb)), "start"=sb[,1], "stop"=sb[,2])
+
   if(is.null(out.file)) return(R)
   save(R, file=out.file)
   if(del.temp){
