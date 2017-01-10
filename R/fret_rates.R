6ab5742507e52a1b4b97d850a0a8f07611d68233
Jean Morrison <jeanm@uchicago.edu>
2017-01-09 18:28:08 -0600

    remove unused options

--- R/fret_rates.R
+++ R/fret_rates.R
@@ -2,11 +2,10 @@
 #'@title Calculate lambda and fdr for each peak
 #'@description Find thresholds for a range of lambda values. Calculate FDR.
 #'@param file.list List of files with output from fret_rates_prelim
-#'@param n.perm Number of permutations
 #'@param fdr.max Maximum fdr to keep data for
 #' @return An object that can be passed to fret_thresholds
 #'@export
-fret_rates <- function(file.list, fdr.max=0.8, temp.dir=".", write.temp=FALSE){
+fret_rates <- function(file.list, fdr.max=0.8){
   #Get info from files
   R <- getobj(file.list[1])
   max1 <- R$m1
@@ -48,6 +47,13 @@ fret_rates <- function(file.list, fdr.max=0.8, temp.dir=".", write.temp=FALSE){
     sum(pmin(r*nbp, max.lambda))
   })
   max1$fdr <- max1$lambda/(1:nrow(max1))
+
+  #Don't keep all of max1 or max.perm since it's huge
+  max1 <- max1[max1$fdr <= fdr.max,]
+  tot.disc <- nrow(max1) ###This is the number of discoveries at fdr.max
+  lam.target <- fdr.max*tot.disc
+  lam.target.pb <- lam.target/sum(segment.bounds$nbp)
+  max.perm <- max.perm[max.perm$lambda_perbase <= lam.target.pb,]
 
   return(list("max1"=max1, "segment.bounds"=segment.bounds,
               "max.perm"=max.perm, "nbp"=nbp, "zmin"=zmin))
