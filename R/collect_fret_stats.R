dbb61463431b5e380121847a827e70bff43ec62e
Jean Morrison <jeanm@uchicago.edu>
2017-04-04 11:56:42 -0500

    Add temporary bits to retrieve permutation number

--- R/collect_fret_stats.R
+++ R/collect_fret_stats.R
@@ -8,9 +8,11 @@
 #' @param out.file Name an output file
 #' @param del.temp Delete the temporary files when done?
 #' @param min.interval.width Minimum interval length when automatically determining interval endpoints
+#' @param n.perm Temporary parameter for compatibility with previous versions.
 #'@export
-collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
+collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk, n.perm=NULL,
                                out.file=NULL, del.temp=FALSE, min.interval.width=NULL){
+  #Do all the files exist?
   fl_exist <- list.files(temp.dir, pattern = temp.prefix, full.names = TRUE)
   fl <- paste0(temp.dir,"/", temp.prefix, ".", which.chunk, ".RData")
   stopifnot(all(fl %in% fl_exist))
@@ -22,6 +24,17 @@ collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
   sts <- R$sts
   chrom <- R$chrom
   zmin <- R$zmin
+  ##TEMPORARY
+  if(is.null(R$n.perm)){
+    if(is.null(n.perm)) stop("Please manually supply n.perm since these files are an older version")
+    else{
+      R$n.perm <- n.perm
+    }
+  }else{
+    n.perm <- R$n.perm
+  }
+  ###
+  #k keeps track of how much information is in the temporary files
   k <- 1
   if("sts.smooth" %in% names(R)){
     sts.smooth <- R$sts.smooth
@@ -33,8 +46,12 @@ collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
   }
   if("mperm" %in% names(R)){
     perm.var <- R$perm.var
+    #This part is also temporary since I originally wasn't storing the permutation number
+    if(ncol(R$mperm)==6){
+      R$mperm$perm <- get_perm_num(R$mperm$ix1, n.perm)
+    }
+    ###
     mperm <- R$mperm
-
     k <- k+1
   }
   for(f in fl[-1]){
@@ -42,11 +59,17 @@ collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
     R <- getobj(f)
     stopifnot(R$chrom == chrom)
     stopifnot(R$zmin == zmin)
+    ##TEMPORARY if statement
+    if(!is.null(R$n.perm)) stopifnot(R$n.perm==n.perm)
+    ##
     sts <- rbind(sts, R$sts)
     if(k > 1) sts.smooth <- rbind(sts.smooth, R$sts.smooth)
     if(k > 2) m1 <- rbind(m1, R$m1)
     if(k > 3){
       perm.var <-rbind(perm.var, R$perm.var)
+      if(ncol(R$mperm)==6){
+        R$mperm$perm <- get_perm_num(R$mperm$ix1, n.perm)
+      }
       mperm <- rbind(mperm, R$mperm)
     }
   }
@@ -73,4 +96,14 @@ collect_fret_stats <- function(temp.dir, temp.prefix, which.chunk,
     for(f in fl) unlink(f)
   }
   return(0)
+}
+
+get_perm_num <- function(ix1, n.perm){
+  N <- length(ix1)
+  xx <- ix1[1:(N-1)] < ix1[2:N]
+  ix <- c(1, which(!xx), N)
+  len <- diff(ix)
+  stopifnot(length(len)==n.perm)
+  perm <- rep(1:n.perm, len)
+  return(perm)
 }
