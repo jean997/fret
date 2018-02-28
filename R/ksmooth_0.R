78088b7fca61fa1dc6b1684090f61e2709da6402
Jean Morrison <jeanm@uchicago.edu>
2018-02-28 13:38:57 -0600

    remove older version of ksmooth_0

--- R/ksmooth_0.R
+++ R/ksmooth_0.R
@@ -1,35 +1,3 @@
-#'@useDynLib fret
-#'@importFrom Rcpp sourceCpp
-
-#'@title Box kernel smoother for integer positions
-#'@description Box kernel for use with DNase-seq and similar data. Assumes missing integer positions have observations of 0.
-#'@param x Vector of positions (integers)
-#'#'@param y Vector of observations to be smoothed
-#'@param xout Vector of output positions
-#'@param bandwidth Smoother bandwidth. Should be an odd integer
-#'@param chunksize Size of chunks to break xout up into. Speeds computation.
-#'@return Vector of smoothed values with length equal to length of xout.
-#'@export
-ksmooth_0_jean <- function(x, y, xout, bandwidth, chunksize){
-  stopifnot(length(x)==length(y))
-  if(!all(floor(x)==x) | !all(floor(xout)==xout)) stop("ksmooth_0 should only be used with integer positions\n")
-  if(!floor(bandwidth)==bandwidth) stop("Please use an odd integer bandwidth with ksmooth_0")
-  if(any(is.na(y))) stop("No missing values please.\n")
-
-  if(bandwidth %% 2 == 0){
-    cat("Warning: ksmooth_0 must use odd bandwidth. Replacing ", bandwidth,
-        " with ", bandwidth + 1, "\n")
-    bandwidth <- bandwidth + 1
-  }
-  if(missing(chunksize)){
-    yout <- ksmooth_0_cpp(x, y, xout, bandwidth)
-    return(yout)
-  }
-
-  yout <- ksmooth_0_stitch(x, y, xout, bandwidth, chunksize)
-  return(yout)
-}
-
 #'@title Box kernel smoother for integer positions
 #'@description Box kernel for use with DNase-seq and similar data. Assumes missing integer positions have observations of 0.
 #'@param x Vector of positions (integers)
