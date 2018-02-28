e4ab2c870c9965665ff07c1eff530e866d37da44
Jean Morrison <jeanm@uchicago.edu>
2018-02-28 17:41:07 -0600

    fix calls to parApply

--- R/stats.R
+++ R/stats.R
@@ -7,16 +7,18 @@ stats1 <- function(Y, x, s0 = 0,  cores, stat_fun, libs = c()){
     }))) %>% dplyr::rename("beta" ="X1", "se" = "X2", "stat" = "X3")
     return(B)
   }
+
+  #cl <- makeCluster(cores, type="FORK")
   cl <- makeCluster(cores, type="PSOCK")
-  clusterExport(cl, varlist = c("Y", "x", "s0", "stat_fun"))
+  clusterExport(cl, varlist = c("stat_fun"), envir=environment())
   if(length(libs) > 0){
     txt <- paste0("library(", libs, ")")
     clusterCall(cl, fun=function(txt){sapply(txt, function(x){eval(parse(text=x))})},
                 txt=txt)
   }
-  B <- parApply(cl, Y, MARGIN=1, FUN=function(y){
-    stat_fun(y, x, s0)
-  })
+  B <- parApply(cl, Y, MARGIN=1, FUN=function(y, myx, s0){
+    stat_fun(y, myx, s0)
+  }, myx=x, s0=s0)
   stopCluster(cl)
   B <- data.frame(t(B)) %>% dplyr::rename("beta" ="X1", "se" = "X2", "stat" = "X3")
   return(B)
@@ -32,18 +34,22 @@ stats_many <- function(Y, X, s0=0, cores, stat_fun, libs=c()){
     }))
     return(B)
   }
+
+  #cl <- makeCluster(cores, type="FORK")
   cl <- makeCluster(cores, type="PSOCK")
-  clusterExport(cl, varlist= c("Y", "X", "s0", "stat_fun"))
+  clusterExport(cl, varlist= c("stat_fun"), envir=environment())
+
   if(length(libs) > 0){
     txt <- paste0("library(", libs, ")")
     clusterCall(cl, fun=function(txt){sapply(txt, function(x){eval(parse(text=x))})},
                 txt=txt)
   }
-  B <- parApply(cl, Y, MARGIN=1, FUN=function(y){
-    apply(X, MARGIN=2, FUN=function(x){
+
+  B <- parApply(cl, X=Y, MARGIN=1, FUN=function(y, myX, s0){
+    apply(myX, MARGIN=2, FUN=function(x, y, s0){
       stat_fun(y, x, s0)[3]
-    })
-  })
+    }, y=y, s0=s0)
+  }, myX = X, s0=s0)
   stopCluster(cl)
   B <- t(B)
   return(B)
